1d0ba691398efce055bb726b184d0b03c7b2e8a7
meandthey <38676400+meandthey@users.noreply.github.com>
2024-09-21 22:15:13 +0900

    ...

--- R/TNFD_CorpData.R
+++ R/TNFD_CorpData.R
@@ -90,6 +90,165 @@ ggplot(data = totalData_GG, aes(x = 중분류코드)) +
 ggplot(data = totalData_GG, aes(x = Full코드)) +
   geom_bar(stat = 'count', width = 0.7)
 
+
+################### [데이터드림림 Data] ###################
+GGdream_Data <- read_delim("../data/데이터드림/2021년업종별사업체현황.csv", locale = locale(encoding = "EUC-KR"), delim = ",", col_names = T)
+GGdream_Data <- GGdream_Data %>%
+  mutate(소재지지번주소 = gsub("번지", "", 소재지지번주소 )) %>%
+  mutate(번호 = row.names(.))
+  # mutate(소재지지번주소 = case_when(
+  #   
+  #   grepl("번지", 소재지지번주소) ~ gsub("번지", "", 소재지지번주소 ),
+  #   TRUE ~ 소재지지번주소
+  #   
+  # ))
+
+
+GGdream_address <- str_split(GGdream_Data$소재지지번주소, " ", simplify = T)
+
+
+
+
+################### [상장법인 Data] ###################
+
+## SSN(486), JWH(445), WSY(537)
+data_SSN <- readxl::read_excel("../data/상장법인/연구원님들/상장기업 검색_송새눈_240921.xlsx", sheet = "상장기업 리스트", col_names = T, skip = 0)
+data_JWH <- readxl::read_excel("../data/상장법인/연구원님들/상장기업 검색_조원호_수정2.xlsx", sheet = "상장기업 리스트", col_names = T, skip = 0) %>%
+  mutate(직원총수 = as.numeric(직원총수))
+data_WSY <- readxl::read_excel("../data/상장법인/연구원님들/상장기업 검색_원수연.xlsx", sheet = "상장기업 리스트", col_names = T, skip = 0) %>%
+  rename(매출액 = `매출액(백만원)`) %>%
+  mutate(직원총수 = as.numeric(직원총수))
+
+
+mergedData_KOS <- data_SSN %>%
+  bind_rows(data_JWH, data_WSY) %>%
+  mutate(회사명 = gsub("_0", "", 회사명)) %>%
+  filter(!grepl("_", 회사명)) %>%
+  mutate(번호 = row.names(.))
+  
+
+mergedData_KOS_address <- str_split(mergedData_KOS$지번주소, " ", simplify = T)
+
+matchNumber_All <- c()
+matchNumberMatrix <- matrix(nrow = nrow(mergedData_KOS_address), ncol = 10)
+for ( i in 1:nrow(mergedData_KOS_address)) {
+  
+  KOS_each <- mergedData_KOS_address[i,]
+  KOS_each <- KOS_each[KOS_each != ""]
+  
+  for ( j in 1:nrow(GGdream_address)) {
+    
+    All_each <- GGdream_address[j,]
+    All_each <- All_each[All_each != ""]
+    
+    result_each <- sum(KOS_each %in% All_each)
+    
+    matchNumber_All[j] <- result_each
+
+  }
+  
+  matchNumber <- which(matchNumber_All == max(matchNumber_All))
+  
+  if (length(matchNumber) >= 10 ) {
+    
+    matchNumberMatrix[i,1] <- 0
+    
+  } else {
+    
+    matchNumberMatrix[i,c(1:length(matchNumber))] <- matchNumber
+    
+  }
+  
+}
+
+matchNumberTibble <- as.tibble(matchNumberMatrix) %>%
+  mutate(번호 = row.names(.))
+
+matchNumberMatrix_onlyOne <- matchNumberTibble %>%
+  filter(is.na(V2),
+         V1 != 0) %>%
+  select(V1, 번호)
+
+final_sampleData <- mergedData_KOS %>%
+  left_join(matchNumberMatrix_onlyOne, by = c("번호")) %>%
+  left_join(GGdream_Data %>% rename(V1 = 번호) %>% mutate(V1 = as.numeric(V1)), by = c("V1"))
+
+  
+
+
+mergedData_KOS 
+GGdream_Data
+
+
+
+
+matchNumberMatrix
+
+matchNumber[matchNumber == max(matchNumber)]
+
+
+
+
+matchNumber <- c()
+for ( i in 1:nrow(GGdream_address)) {
+  
+  All_each <- GGdream_address[i,]
+  All_each <- All_each[All_each != ""]
+  
+  result_each <- sum(KOS_1 %in% All_each)
+  
+  matchNumber[i] <- result_each
+  #matchNumber[i, "AA"] <- result_each
+  
+  
+}
+
+
+
+##### 비교교 #####
+KOS_1 <- mergedData_KOS_address[1,]
+KOS_1 <- KOS_1[KOS_1 != ""]
+#All_1[All_1 == ""] <- NA
+
+
+All_1 <- GGdream_address[1,] 
+All_1 <- All_1[All_1 != ""]
+#KOS_1[KOS_1 == ""] <- NA
+
+KOS_1 %in% All_1
+
+
+All_1 %>%
+  mutate(wtdAdrs = )
+
+KOS_1 <- mergedData_KOS_address[1,]
+
+
+for ( i in 1:nrow())
+
+
+
+
+
+
+
+
+
+
+
+
+A <- "A B CC CAE FE"
+B <- "A BC CD"
+C <- "AA BB CCC CDE"
+ABC <- t(data.frame( A, B, C))
+
+
+AA <- c("사과", "배", "감")
+BB <- c("오렌지", "사과", "포도", "배")
+AA %in% BB
+
+
+
 ################### [상장법인 Data] ###################
 
 rawData_KOS <- readxl::read_excel("../data/상장법인/상장법인목록.xlsx", sheet = "데이터", col_names = T, skip = 0) %>%
@@ -119,17 +278,19 @@ KOS_data <- rawData_KOS %>%
 
 
 
-#### Test ################### [상장법인 Data] ###################
-read.csv("../data/데이터드림/2021년업종별사업체현황.csv")
-read_csv("../data/데이터드림/2021년업종별사업체현황.csv")
+################### [상장법인 Data] ###################
 
-read_delim("../data/데이터드림/2021년업종별사업체현황.csv", locale = locale(encoding = "EUC-KR"), delim = ",", col_names = F)
 
 data_SSN <- readxl::read_excel("../data/상장법인/연구원님들/상장기업 검색_송새눈.xlsx", sheet = "상장기업 리스트", col_names = T, skip = 0)
 
 data_SSN
 
 
+
+read.csv("../data/데이터드림/2021년업종별사업체현황.csv")
+read_csv("../data/데이터드림/2021년업종별사업체현황.csv")
+
+<- read_delim("../data/데이터드림/2021년업종별사업체현황.csv", locale = locale(encoding = "EUC-KR"), delim = ",", col_names = F)
 
 
 
